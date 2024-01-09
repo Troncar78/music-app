2ed3fa2dff4d1b9567d7d6eec6eb9bdd6320f28b
Guillaume LAMIC <75310445+Troncar78@users.noreply.github.com>
2024-01-09 09:26:53 +0100

    Initial commit

---
+++ resources/js/Pages/Track/Create.vue
@@ -0,0 +1,26 @@
+<template>
+    <MusicLayout>
+        <template #title>
+            Créer une Musiques
+        </template>
+
+        <template #action> 
+            <Link :href="route('tracks.index')">
+                Retour au menu </Link>
+        </template>
+        <template #content>
+
+        </template>
+    </MusicLayout>
+</template>
+
+<script>
+import MusicLayout from '@/Layouts/MusicLayout.vue'
+import { Link } from '@inertiajs/vue3';
+export default {
+    components: {
+        MusicLayout,
+        Link
+    },
+}
+</script>
